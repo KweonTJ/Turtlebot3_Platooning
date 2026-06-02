d2e792665b1f39914d424dc7237e0b8a340eec0c
KweonTJ <kweontj0701@naver.com>
2026-06-02 17:57:47 +0900

    auto: update 2026-06-02 17:57:47

---
+++ follower_bringup/launch/teleop_follow.launch.py
@@ -0,0 +1,44 @@
+# Copyright 2026 ktj
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare("follower_bringup"),
+                    "launch",
+                    "follower_system.launch.py",
+                ])
+            ]),
+            launch_arguments={
+                "use_camera": "false",
+                "start_vision": "false",
+                "monitor_video_enabled": "false",
+                "start_platooning": "true",
+                "start_safety": "true",
+                "start_base_driver": "true",
+                "publish_robot_description": "true",
+                "start_joint_state_publisher": "false",
+                "start_rviz": "false",
+            }.items(),
+        ),
+    ])
